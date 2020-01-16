1f88d39137450cef0f3104f91a285f2fa845f94b
Panagiotis Tsilifis <pantsili@gmail.com>
2020-01-16 18:21:43 -0500

    Updated toy2.py

--- unittests/toy2.py
+++ unittests/toy2.py
@@ -3,8 +3,10 @@ import scipy.stats as st
 import matplotlib.pyplot as plt 
 
 x = st.norm.rvs(size = (1000,))
-y = np.cos(x) + 0.2*st.norm.rvs()
+y1 = np.sin(x) + 0.2*st.norm.rvs(size = (1000,))
+y2 = np.cos(x) + 0.2*st.norm.rvs(size = (1000,))
 
-plt.plot(x, y, 'x')
+plt.plot(x, y1, 'x')
+plt.plot(x, y2, '+')
 plt.show()
 
